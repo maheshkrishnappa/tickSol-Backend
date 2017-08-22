022ae04b256041a05b2a24e34c50309b19ebd2b2
rmit-s3548721-kanchan-gurung <s3548721@student.rmit.edu.au>
2017-08-22 15:05:24 +0530

    Venue controller modification

---
+++ app/Http/Controllers/VenueController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Venue;
+
+class VenueController extends Controller
+{
+
+    public function index()
+    {
+        $venues = Venue::all(array('id', 'name', 'address', 'capacity'));
+
+        foreach ($venues as $venue) {
+            $result[] = array('name' => $venue->name, 'address' => $venue->address, 'capacity' => $venue->capacity);
+        }
+
+        return response()->json($venues);
+    }
+
+    public function show($id)
+    {
+        $venue = new Venue();
+        $result = $venue->findOrFail($id, array('id', 'name', 'address', 'capacity'));
+
+        return response()->json($result);
+    }
+
+   
+
+    public function store(Request $data)
+    {
+        $venue = new Venue();
+        $newVenue = $venue->create($data->all());
+        //$venue->name = $data->name;
+        //$venue->address = $data->address;
+        //$venue->capacity = $data->capacity;
+        //$venue->save();
+
+        return 201;
+    }
+
+    public function update(Request $data, $id)
+    {
+        $venueModel = new Venue();
+        $venue = $venueModel->find($id);
+        if($venue == null)
+        {
+            return 404;
+        }
+
+        else{
+            $venue->update($data->all());
+            return $venue;
+        }
+    }
+
+    public function delete($id)
+    {
+        $venueModel = new Venue();
+        $venue = $venueModel->findOrFail($id);
+        $venue->delete();
+
+        return 204;
+    }
+}
