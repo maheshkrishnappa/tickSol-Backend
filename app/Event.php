38367b8a6c503bd189613c78dcf9c8402cb59243
Vishweshmn <30775751+Vishweshmn@users.noreply.github.com>
2017-10-20 18:07:27 +1100

    Updated Event master VMN.

--- app/Event.php
+++ app/Event.php
@@ -22,4 +22,10 @@ class Event extends Model
         return $this->belongsToMany('App\Venue')
                ->withPivot(['conduct_date', 'start_time', 'end_time']);
     }
+
+    public function venues2()
+    {
+        return $this->belongsToMany('App\Venue')
+               ->withPivot(['conduct_date', 'start_time', 'end_time']);
+    }
 }
